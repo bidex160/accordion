a750fe502d845025b86cd9d1d859b85750ca806d
Tajudeen Wariz <wtajudeen@sysserve.com>
2024-10-20 01:19:53 +0100

    accordion

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import questions from "./data";
+import Questions from "./Questions";
+function App() {
+  return (
+    <main>
+      <Questions questions={questions} />
+    </main>
+  );
+}
+
+export default App;
